f0c81ab58ca2baee6a7ab2258712db3cf2d1a56e
Nikunj Banker <nikunj.banker@decos.com>
2020-03-16 13:29:57 +0100

    Add project files.

---
+++ OrderManagementWeb/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Interfaces;
+using OrderManagement.Models;
+
+namespace OrderManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private IOrderManager orderManager;
+
+        public OrderController(IOrderManager orderMngr)
+        {
+            orderManager = orderMngr;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post(Order order)
+        {
+            return Ok(orderManager.Transmit(order));
+        }
+    }
+}
